56177c561dbc389be563e9a7b01cda12c56f439b
Tuan SPK <tuanspk@tuans-mac.local>
2018-04-20 09:55:57 +0700

    first commit

--- SampleCalculatorApp/ViewController.swift
+++ SampleCalculatorApp/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var isFirstNumber = true
+    var operation = ""
+    var soThu1:Double = 0
+    
+    var is1 = true
+    
+    var displayValue:Double {
+        get {
+            return NumberFormatter().number(from: label.text!)!.doubleValue
+        } set {
+            label.text = String(newValue)
+            operation = ""
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +38,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func chuSo(_ sender: Any) {
+        if is1 {
+            isFirstNumber = true
+        }
+        if isFirstNumber {
+            label.text = (sender as AnyObject).currentTitle!
+        } else {
+            label.text! += (sender as AnyObject).currentTitle!!
+        }
+        isFirstNumber = false
+        is1 = false
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        displayValue = 0
+        label.text = String("")
+    }
+    
+    @IBAction func calculator(_ sender: Any) {
+        is1 = true
+        switch operation {
+        case "+":
+            displayValue += soThu1
+            break
+        case "-":
+            displayValue = soThu1 - displayValue
+            break
+        case "*":
+            displayValue *= soThu1
+            break
+        case "/":
+            displayValue = soThu1 / displayValue
+            break
+        default: break
+        }
+    }
+    
+    @IBAction func toanTu(_ sender: Any) {
+        operation = (sender as AnyObject).currentTitle!!
+        soThu1 = displayValue
+        isFirstNumber = true
+    }
 }
 
