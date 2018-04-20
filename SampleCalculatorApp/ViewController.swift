0f98ccb86410e9c949175d540774bc65d931cdc4
Tuan SPK <tuanspk@tuans-mac.local>
2018-04-20 07:59:52 +0700

    Initial Commit

---
+++ SampleCalculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCalculatorApp
+//
+//  Created by Tuan SPK on 4/20/18.
+//  Copyright © 2018 Tuan SPK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
